9f41a26d1138e1cc6b9231dcd621f945c0a71eb2
oscar <oscar.jendralski@gmail.com>
2026-08-26 14:48:34 +0200

    refactor(Statement): refactored statement.py to feature logical expression trees with expression graphs as leave nodes

--- Statement.py
+++ Statement.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import Generator, Dict
+from functools import cached_property
+from typing import Generator, Dict, Union
 from ExpressionTree import Node
 from dataclasses import dataclass, field, replace
 from enum import Enum
@@ -11,76 +12,47 @@ class Relation(Enum):
     LEQ = '<='
     GEQ = '>='
 
+class LogicalOperation(Enum):
+    AND = 'and'
+    OR = 'or'
 
-@dataclass
+
+@dataclass(frozen=True)
 class Statement:
-    expression1: Node
-    expression2: Node
-    relation: Relation
+    """
+    Statements are tree strucutures (ast) that will be used to express logical relation of different nodes terms.
+    Leave nodes must be terms (so of type node of the expression trees).
+    Logical functions such as (land and lor) are nodes in that graph with two child nodes.
+    Statements or leave nodes must be passed into a relation to make them into boolean expressions.
+    """
+    node_function: Union[LogicalOperation, Relation, Node]
+    child_right: Union[Statement, None]
+    child_left: Union[Statement, None]
 
-    def __call__(self, other: Node, print_trace: bool = False) -> Generator[Node]:
-        if print_trace: print(f"Checking {str(self)}({other})")
-        for res, parent_res in other.primitive_contains(self.expression1, print_trace=print_trace):
-            if print_trace: print(f"Checking subtree {res}")
-            mapping, valid = self.expression1.get_mappings_dict_for_replacement(res)
-            if not valid:
-                continue
-            final_sub_expression = self.expression2.remap_objects(mapping)
-            if print_trace: print(f"Remapped subtree to {final_sub_expression}")
-            # Reorder final_sub_expression into original tree
-            if parent_res is None:
-                if print_trace: print("Subtree is final-tree")
-                yield final_sub_expression
-            else:
-                new_child_nodes = []
-                for child_chain in parent_res.child_nodes:
-                    new_chain = []
-                    for child_node, weight in child_chain:
-                        if child_node == res:
-                            new_chain.append((final_sub_expression, weight))
-                        else:
-                            new_chain.append((child_node, weight))
-                    new_child_nodes.append(new_chain)
+    def __post_init__(self):
+        """
+        Checks if definition is valid.
+        If children are nodes, then node_function must be a realation.
+        If the node_function is a logical operation, then child nodes can't be expression graphs
+        If the node_function is an expression graph, they must be a leave node
+        """
+        if isinstance(self.node_function, LogicalOperation):
+            if not self.child_right or not self.child_left:
+                raise Exception("Logical operations must have children nodes")
+            if isinstance(self.child_right.node_function, Node) or isinstance(self.child_left.node_function, Node):
+                raise Exception("Logical operations must have boolean children")
 
-                parent_copy = Node(math_object=parent_res.math_object, child_nodes=new_child_nodes)
+        if isinstance(self.node_function, Relation):
+            if not self.child_right or not self.child_left:
+                raise Exception("Relations must have children nodes")
+            if not isinstance(self.child_right.node_function, Node) or not isinstance(self.child_left.node_function, Node):
+                raise Exception("Relation must have children of type nodes (so an expression graph)")
 
-                if print_trace: print(f"Reordered subtree into main tree to {parent_copy}")
-                yield parent_copy
-        else:
-            if print_trace: print("Done \n")
+        if isinstance(self.node_function, Node):
+            if not self.is_leave_node:
+                raise Exception("Expression graphs can't have child nodes")
 
-    def apply_inverse(self, other: Node, print_trace: bool = False) -> Generator[Node]:
-        """Inverse direction"""
-        if print_trace: print(f"Checking {self}^-1({other})")
-        for res, parent_res in other.primitive_contains(self.expression2, print_trace=print_trace):
-            if print_trace: print(f"Checking subtree {res}")
-            mapping, valid = self.expression2.get_mappings_dict_for_replacement(res, print_trace=print_trace)
-            if not valid:
-                continue
-            final_sub_expression = self.expression1.remap_objects(mapping)
-            if print_trace: print(f"Remapped subtree to {final_sub_expression}")
-            # Reorder final_sub_expression into original tree
-            if parent_res is None:
-                if print_trace: print("Subtree is final-tree")
-                yield final_sub_expression
-            else:
-                new_child_nodes = []
-                for child_chain in parent_res.child_nodes:
-                    new_chain = []
-                    for child_node, weight in child_chain:
-                        if child_node == res:
-                            new_chain.append((final_sub_expression, weight))
-                        else:
-                            new_chain.append((child_node, weight))
-                    new_child_nodes.append(new_chain)
-
-                parent_copy = Node(math_object=parent_res.math_object, child_nodes=new_child_nodes)
-
-                if print_trace: print(f"Reordered subtree into main tree to {parent_copy}")
-                yield parent_copy
-        else:
-            if print_trace: print("Done \n")
-
-    def __str__(self):
-        return f'{str(self.expression1)} {self.relation} {self.expression2}'
-
+    @cached_property
+    def is_leave_node(self) -> bool:
+        """ Retuns if self is root node, thus doesn't have any child nodes """
+        return self.child_right is None and self.child_left is None
